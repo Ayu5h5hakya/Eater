0338011b9cfeb960880fcb5f2a693952a2b396a3
Ayu5h5hakya <qwerty.ayush5@gmail.com>
2024-09-10 14:25:05 +0545

    setup Eater

---
+++ app/src/main/java/com/example/eater/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.eater
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.eater.ui.theme.EaterTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            EaterTheme {
+                Eater()
+            }
+        }
+    }
+}
+
+@Composable
+fun Eater() {
+
+}
